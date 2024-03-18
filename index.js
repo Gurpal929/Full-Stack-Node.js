c3135bafea78bdda61456c7a0161bc838a64ef8f
Jagdeep Kaur <jagdeepk0908@gmail.com>
2024-03-18 18:28:07 -0400

    Success in displaying G2_Test and G_Test when user login as driver.

--- index.js
+++ index.js
@@ -1,232 +1,174 @@
 // ############ MODULES ##########################
 
+const express = require("express");
+const ejs = require("ejs");
+const bcrypt = require("bcryptjs");
+const path = require("path");
 
-const express = require('express')     
-const ejs = require('ejs')              
-const bcrypt = require('bcryptjs');
-const path = require('path')
+const mongoose = require("mongoose");
 
+const User = require("./models/userModel");
 
-const mongoose=require('mongoose')
-
-
-const User=require('./models/userModel')
-
-
-
-// ------ Connect to database 
+// ------ Connect to database
 
 //mongo db connection string
-const mongoCS='mongodb+srv://Sahota:Jattsahota246@Sahota.qutvfqj.mongodb.net/';
+const mongoCS =
+  "mongodb+srv://Sahota:Jattsahota246@Sahota.qutvfqj.mongodb.net/";
 
 try {
-    const connection = mongoose.connect(mongoCS)
-    // const User = require('./models/userModel')
-    console.log("Mongo DB Conncected")
-} catch(err){
-    console.log("MongoDB Connecting Error!!")
-    //TODO: If db is not connected, should the program go further or end here ?
+  const connection = mongoose.connect(mongoCS);
+  // const User = require('./models/userModel')
+  console.log("Mongo DB Conncected");
+} catch (err) {
+  console.log("MongoDB Connecting Error!!");
+  //TODO: If db is not connected, should the program go further or end here ?
 }
 
-// ############ CREATE APPLICATION ########################## 
-const app = express()      
+// ############ CREATE APPLICATION ##########################
+const app = express();
 
 // ############ MIDDLEWARE ##########################
 
-app.use(express.static('public'))               
-app.use(express.json())                         
-app.use(express.urlencoded({extended:true}))    
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ############ VIEW ENGINE ##########################
 
-app.set('view engine','ejs')    
-
+app.set("view engine", "ejs");
 
 // ############ ROUTES ##########################
 
 // 1. Dashboard on GET
-app.get('/',(req,res)=>{
-    res.render('dashboard')
+app.get("/", (req, res) => {
+  res.render("dashboard");
 });
 
 //2. Login route on GET
-app.get('/login',(req,res)=>{
-    res.render('login')
- });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-
-
- //3. G2 route on GET
-app.get('/G2',(req,res)=>{
-    res.render('G2_Test')}) ;
+//3. G2 route on GET
+app.get("/G2", (req, res) => {
+  res.render("G2_Test");
+});
 
 //4. G route on GET
-app.get('/G',(req,res)=>{
-    res.render('G_Test')
+app.get("/G", (req, res) => {
+  res.render("G_Test");
 });
 
 //5. Add new user on GET
-
-app.get('/add-new-user',(req,res)=>{
- res.render('newUser')
+app.get("/add-new-user", (req, res) => {
+  res.render("newUser");
 });
 
 //6. SignUp route on GET
-app.get('/signup',(req,res)=>{
-    res.render('signup')
- });
-
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 //6. Add new user on POST (Route to handle form submission and save user data)
 
-    app.post('/add-new-user', async (req, res) => {
-    try {
-        
-        const newUser = new User(req.body);
-
-
-        await newUser.save()
-            .then(user => res.render('G2_Test', {user: user}))  
-            .catch(err => res.render('G2_Test', {error: err}))  
-
-    } catch (error) {
-        
-        console.error('Error adding user:', error);
-  
-        res.render('G2_Test', {error: error})   
-    }
+app.post("/add-new-user", async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser
+      .save()
+      .then((user) => res.render("G2_Test", { user: user }))
+      .catch((err) => res.render("G2_Test", { error: err }));
+  } catch (error) {
+    console.error("Error adding user:", error);
+    res.render("G2_Test", { error: error });
+  }
 });
 
-
 //7. Find user on GET (retrieve data from the database)
-app.get('/findUser',async (req,res)=>{
-    
-    console.log(`Finding license number ${req.query.licenseNo} ... `)
-    const result= await User.find({  licenseNo: req.query.licenseNo });
-    console.log(`Results found: ${result.length}`)
-    console.log(`Results: ${result}`)
+app.get("/findUser", async (req, res) => {
+  console.log(`Finding license number ${req.query.licenseNo} ... `);
+  const result = await User.find({ licenseNo: req.query.licenseNo });
+  console.log(`Results found: ${result.length}`);
+  console.log(`Results: ${result}`);
 
-    res.render('G_Test',{result: result})
-    }
-  
-);
-
+  res.render("G_Test", { result: result });
+});
 
 //8. update car details on GET
-app.get('/updateCarDetails', async (req, res) => {
-    const result= await User.findOne({  _id: req.query.ID });
-    res.render('updateCarDetails', result)
-})
+app.get("/updateCarDetails", async (req, res) => {
+  const result = await User.findOne({ _id: req.query.ID });
+  res.render("updateCarDetails", result);
+});
 //9. Update car details on POST (update database record)
-app.post('/updateCarDetails', async (req, res) => {
-    console.log(`Request body Id: ${req.body._id}`)
-    var update_status=" "
-    const result= await User.findByIdAndUpdate(req.body._id, req.body , {new: true})
-    if(typeof(result) == undefined){
-        update_status="failed"
-    }else{
-            update_status="success"
-    }
-    result["update_status"]=update_status;
-   
-    console.log(`Result ${result}`)
-    res.render('updateCarDetails', result)
-})
+app.post("/updateCarDetails", async (req, res) => {
+  console.log(`Request body Id: ${req.body._id}`);
+  var update_status = " ";
+  const result = await User.findByIdAndUpdate(req.body._id, req.body, {
+    new: true,
+  });
+  if (typeof result == undefined) {
+    update_status = "failed";
+  } else {
+    update_status = "success";
+  }
+  result["update_status"] = update_status;
+
+  console.log(`Result ${result}`);
+  res.render("updateCarDetails", result);
+});
 
 //10. Signup
-app.post('/signupUser', async (req, res) => {
-    try {
-        const { username, password, userType } = req.body;
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-
-        const newUser = new User({ username, password, userType});
-        await newUser.save()
-        // res.status(201).json({ message: 'User created successfully' });
-        .then(user => res.render('login', {user: user}))  
-            .catch(err => res.render('login', {error: err}))  
-    } catch (error) {
-        console.error('Error signing up:', error);
-        res.status(500).json({ message: 'An error occurred while signing up' });
+app.post("/signupUser", async (req, res) => {
+  try {
+    const { username, password, userType } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
     }
+
+    const newUser = new User({ username, password, userType });
+    await newUser
+      .save()
+      // res.status(201).json({ message: 'User created successfully' });
+      .then((user) => res.render("login", { user: user }))
+      .catch((err) => res.render("login", { error: err }));
+  } catch (error) {
+    console.error("Error signing up:", error);
+    res.status(500).json({ message: "An error occurred while signing up" });
+  }
 });
 
 //11.Login
-app.post('/loginUser', async (req, res) => {
-    const { login_username, login_password,} = req.body;
-    console.log(login_username,login_password);
-    let userLogin=false;
+app.post("/loginUser", async (req, res) => {
+  const { login_username, login_password } = req.body;
+  let userLogin = false;
 
-    // Check if the user exists in the database
-  
-    // const hash = await bcrypt.hash(login_password, 10);
-    // console.log(hash);
-    const user = await User.findOne({ username: login_username});
-    console.log(user);
-    console.log(user.password);
-  
+  // Check if the user exists in the database
+  const user = await User.findOne({ username: login_username });
 
-
-    // Load hash from your password DB.
-   await bcrypt.compare(login_password, user.password, function(err, result) {
+  // Load hash from your password DB.
+  bcrypt.compare(login_password, user.password, function (err, result) {
     // result == true
-      if(!err){
-        console.log(' Result ' + result);
-        userLogin=result;
-      }else{
-          console.log(' Error ' + err);
+    if (!err) {
+      console.log(" Result " + result);
+      userLogin = result;
+    } else {
+      console.log(" Error " + err);
+    }
+
+    if (!userLogin) {
+      res.render("login", req.body);
+    } else {
+      // User is logging in again
+      if (user.userType === "Driver") {
+        res.render("dashboard", { user: user });
+      } else {
+        res.send("userType is not Driver");
       }
-
-
-      if (!userLogin) {
-        // res.send('unsuccesful')
-        res.render('login', req.body);
-    }else {
-        // User is logging in again
-      
-        //res.send('Login Successfull');
-        // If login is successful, render the login success page and display the G2_Test link again
-        // res.render('dashboard', { username: login_username, displayG2Test: true });
-        // If UserType is Driver, show navigation options for G2_page and G_page
-      if (user.userType === 'Driver') {
-        // res.render('dashboard', { user });
-        res.render('dashboard',{user:user});
-        } else {
-        res.send('userType is not Driver');
-      }
-      }
-      
-      
-    });
-    console.log(userLogin);
-    console.log(userLogin);
-    
-
-    
-
-    // if (!userLogin) {
-    //     // res.send('unsuccesful')
-    //     res.render('login', req.body);
-    // }else {
-    //     // User is logging in again
-      
-    //     //res.send('Login Successfull');
-    //     // If login is successful, render the login success page and display the G2_Test link again
-    //     // res.render('dashboard', { username: login_username, displayG2Test: true });
-    //     // If UserType is Driver, show navigation options for G2_page and G_page
-    //   if (user.userType === 'Driver') {
-    //     // res.render('dashboard', { user });
-    //     res.redirect('dashboard');
-    //     } else {
-    //     res.send('userType is not Driver');
-    //   }
-    //   }
+    }
+  });
 });
 
-app.listen(4000,()=>{
-    console.log(`Application link : http://localhost:4000/`)
-})
-
-
+app.listen(4000, () => {
+  console.log(`Application link : http://localhost:4000/`);
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,62 @@
-const  mongoose=require('mongoose')
-const bcrypt = require('bcrypt');
-const Schema = mongoose.Schema
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   // username: { type: String, required: true, unique: true },
   // password: { type: String, required: true },
-    username:String,
-    password:String,
-     userType: { type: String, enum: ['Driver', 'Examiner', 'Admin'], required: true, default: 'Driver' },
-    // userType:'Driver',
-    firstname: String,
-    lastname: String,
-    licenseNo: String,
-    DOB:String,
-    age: Number,
-    car_make: String,
-    car_model: String,
-    car_year: String,
-    car_plateno: String
-    // userType: { type: String, enum: ['Driver', 'Examiner', 'Admin'], required: true, default: 'Driver' },
-    // firstname: { type: String, default: 'John' },
-    // lastname: { type: String, default: 'Doe' },
-    // licenseNo: { type: String, default: 'A12345' },
-    // car_make: { type: String, default: 'Toyota' },
-    // car_model: { type: String, default: 'Corolla' },
-    // car_year: { type: String, default: '2022' },
-    // car_plateno: { type: String, default: 'XYZ123' }
-  });
-
-  // Hash password before saving
-userSchema.pre('save', async function(next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-    const hash = await bcrypt.hash(user.password, 10);
-    user.password = hash;
-    next();
+  username: String,
+  password: String,
+  userType: {
+    type: String,
+    enum: ["Driver", "Examiner", "Admin"],
+    required: true,
+    default: "Driver",
+  },
+  // userType:'Driver',
+  firstname: String,
+  lastname: String,
+  licenseNo: String,
+  DOB: String,
+  age: Number,
+  car_make: String,
+  car_model: String,
+  car_year: String,
+  car_plateno: String,
+  // userType: { type: String, enum: ['Driver', 'Examiner', 'Admin'], required: true, default: 'Driver' },
+  // firstname: { type: String, default: 'John' },
+  // lastname: { type: String, default: 'Doe' },
+  // licenseNo: { type: String, default: 'A12345' },
+  // car_make: { type: String, default: 'Toyota' },
+  // car_model: { type: String, default: 'Corolla' },
+  // car_year: { type: String, default: '2022' },
+  // car_plateno: { type: String, default: 'XYZ123' }
 });
 
-const User = mongoose.model('User',userSchema)
 
-module.exports=User
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // Hash password before saving
+  if (user.isModified("password")) {
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+  }
+
+  // Hash licenseNo before saving
+  if (user.isModified("licenseNo")) {
+    const hash = await bcrypt.hash(user.licenseNo, 10);
+    user.licenseNo= hash;
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+
+// if (!user.isModified("password")) return next();
+//   const hash = await bcrypt.hash(user.password, 10);
+//   user.password = hash;
+//   next();
+// }
