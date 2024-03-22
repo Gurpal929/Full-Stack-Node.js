f19414323da565d61baae225db8e39438bcbb013
Jagdeep Kaur <jagdeepk0908@gmail.com>
2024-03-21 20:32:14 -0400

    Logut functionality added

--- models/userModel.js
+++ models/userModel.js
@@ -3,8 +3,6 @@ const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  // username: { type: String, required: true, unique: true },
-  // password: { type: String, required: true },
   username: String,
   password: String,
   userType: {
@@ -13,7 +11,6 @@ const userSchema = new Schema({
     required: true,
     default: "Driver",
   },
-  // userType:'Driver',
   firstname: String,
   lastname: String,
   licenseNo: String,
@@ -23,14 +20,6 @@ const userSchema = new Schema({
   car_model: String,
   car_year: String,
   car_plateno: String,
-  // userType: { type: String, enum: ['Driver', 'Examiner', 'Admin'], required: true, default: 'Driver' },
-  // firstname: { type: String, default: 'John' },
-  // lastname: { type: String, default: 'Doe' },
-  // licenseNo: { type: String, default: 'A12345' },
-  // car_make: { type: String, default: 'Toyota' },
-  // car_model: { type: String, default: 'Corolla' },
-  // car_year: { type: String, default: '2022' },
-  // car_plateno: { type: String, default: 'XYZ123' }
 });
 
 
